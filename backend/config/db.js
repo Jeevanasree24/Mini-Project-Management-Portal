0efd9ff749a359c99e0c2ec25ad55cdbaa98d290
Jeevanasree24 <jeevanasreeperisetla@gmail.com>
2026-06-20 21:46:32 +0530

    modiefies db.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -7,6 +7,9 @@ const pool = mysql.createPool({
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME || 'project_management',
   port: process.env.DB_PORT || 3306,
+  ssl: {
+    rejectUnauthorized: false
+  },
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0
